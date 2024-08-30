3a5e47d2653b4b6667c3e76fc994f72ec0456c9c
Vanina Castelo <v.i.castelo@gmail.com>
2024-08-30 15:14:25 -0300

    "Evitar propagación del evento click del botón"

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.getElementById('miDiv').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+const boton = document.getElementById('miBoton');
+boton.addEventListener('click', (event) => {
+    event.stopPropagation();
+    alert('¡Botón clickeado!');
+});
